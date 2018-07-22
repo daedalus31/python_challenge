17578b04748707afe5f3d9f6caece5b837e6a817
daedalus31 <mat.marczykowski@gmail.com>
2018-07-22 17:28:01 +0200

    challenge 8 completed

---
+++ integrity_challenge.py
@@ -0,0 +1,12 @@
+import bz2
+
+username = b"BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084"
+password = b"BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08"
+
+
+def decipher(byte_string):
+    return bz2.decompress(byte_string).decode('utf-8')
+
+
+print("username:", decipher(username))
+print("password:", decipher(password))
